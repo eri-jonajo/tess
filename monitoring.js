c23b9ac670279b1c75f1757ea5599e657f770557
Eri Indrawan <eri.indrawan@jonajo.com>
2018-09-20 08:34:49 +0700

    initial

---
+++ monitoring.js
@@ -0,0 +1,79 @@
+require('dotenv').config();
+var mysql = require('mysql');
+const utils = require("./utils.js");
+
+var DB_TC = mysql.createConnection({
+    host: process.env.TC_HOST,
+    user: process.env.TC_USER,
+    password: process.env.TC_PASS,
+    database: process.env.TC_DB
+});
+
+var DB_DEV = mysql.createConnection({
+    host: process.env.DEV_HOST,
+    user: process.env.DEV_USER,
+    password: process.env.DEV_PASS,
+    database: process.env.DEV_DB
+});
+
+var DB_PROD = mysql.createConnection({
+    host: process.env.PROD_HOST,
+    user: process.env.PROD_USER,
+    password: process.env.PROD_PASS,
+    database: process.env.PROD_DB
+});
+
+var emailBody = 'Batch Failure:\n';
+var hasError = false;
+var step = 0;
+function next_process() {
+    switch (step) {
+        case 0:
+            console.log('Checking TC');
+            check_failure(DB_TC);
+            break;
+        case 1:
+            console.log('Checking DEV')
+            check_failure(DB_DEV);
+            break;
+        case 2:
+            console.log('Checking PROD')
+            check_failure(DB_PROD);
+            break;
+        case 3:
+            if (hasError) {
+                console.log('Reporting..');
+                utils.send_slack({ text: emailBody });
+                //utils.send_email();
+            }
+            process.exit();
+            break;
+    }
+    step++;
+};
+
+function check_failure(con) {
+    con.connect(function (err) {
+        if (err) utils.send_slack({ channel: process.env.SLACK_ERI, text: 'ERCON: ' + con });
+
+        con.query(`CALL bat_report_failure();`, function (err, res, fields) {
+            if (err) utils.send_slack({ channel: process.env.SLACK_ERI, text: 'ERBAT: ' + con + ': ' + bat_report_failure });
+
+            var result = res[0];
+            if (result.length) {
+                hasError = true;
+                for (var x in result) {
+                    emailBody += result[x]['server_name'] + ' ' + result[x]['script_name'] + ' at ' + result[x]['start_time'] + '\n';
+                }
+
+                next_process();
+            } else {
+                console.log('No Failure');
+                next_process();
+            }
+        });
+    });
+}
+
+
+next_process();
---
+++ utils.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+var nodemailer = require('nodemailer');
+var mysql = require('mysql');
+const { WebClient } = require('@slack/client');
+
+module.exports = {
+    send_email: function () {
+        nodemailer.createTestAccount((err, account) => {
+            // create reusable transporter object using the default SMTP transport
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: process.env.EMAIL_SENDER,
+                    pass: process.env.EMAIL_SENDERPASS
+                }
+            });
+
+            console.log(emailBody);
+            // setup email data with unicode symbols
+            var mailOptions = {
+                from: '"Error Checker" <tess@theofe.com>', // sender address
+                to: process.env.EMAIL_TOALL, // list of receivers
+                subject: 'Batch Error', // Subject line
+                text: emailBody, // plain text body
+                html: emailBody.replace(/\n/g, '<br/>\n') // html body
+            };
+
+            // send mail with defined transport object
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    return console.log(error);
+                }
+            });
+        });
+    },
+
+    send_slack: function (opt) {
+        opt = Object.assign({
+            channel: process.env.SLACK_ALL,
+            text: ''
+        }, opt)
+
+        console.log('Sending to slack..');
+        var token = process.env.SLACK_TOKEN;
+        var slack = new WebClient(token);
+
+        // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
+
+        // See: https://api.slack.com/methods/chat.postMessage
+        slack.chat.postMessage({ channel: opt.channel, text: opt.text, as_user: false, username: 'TESS' })
+            .then((res) => {
+                // `res` contains information about the posted message
+                console.log('Message sent: ', res.ts);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+}
