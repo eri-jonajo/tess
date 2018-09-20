416ff5c3d9383461660326e2a8d8e1b8a7e78986
Eri Indrawan <eri.indrawan@jonajo.com>
2018-09-20 08:44:26 +0700

    talk.js enable enter

--- talk.js
+++ talk.js
@@ -3,8 +3,8 @@ const utils = require("./utils.js");
 
 //utils.send_slack({ text: 'test' });
 if (process.argv.length == 3)
-    utils.send_slack({ text: process.argv[2] });
+    utils.send_slack({ text: process.argv[2].replace(/\\n/ig, '\n') });
 else if (process.argv.length == 4)
-    utils.send_slack({ channel: process.env['SLACK_' + process.argv[2]], text: process.argv[3] });
+    utils.send_slack({ channel: process.env['SLACK_' + process.argv[2]], text: process.argv[3].replace(/\\n/ig, '\n') });
 else
     console.log(process.env.SLACK_ERI);
